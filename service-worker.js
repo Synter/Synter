66d20146744037aaabced793f2b0bad5e9bc687f
Synter <49393232+Synter@users.noreply.github.com>
2026-09-25 10:47:47 +0200

    Bump cache for iOS ZIP sharing fix

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE='raporty-v5';
+const CACHE='raporty-v6';
 const CORE=[
   './','./index.html','./manifest.json','./icon.svg',
   'https://cdnjs.cloudflare.com/ajax/libs/jspdf/2.5.1/jspdf.umd.min.js',
